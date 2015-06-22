dba7da8beef4e0e691d20f69e0e0d14ad5377ae0
Emmet Broaders <broadere@tcd.ie>
2015-06-22 21:06:51 +0100

    Fixed the nav, stop fucking with stuff

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -4,7 +4,6 @@
 Rails.application.config.assets.version = '1.0'
 Rails.application.config.assets.precompile += %w( jquery.js )
 Rails.application.config.assets.precompile += %w( jquery.sticky.js )
-
 # Add additional assets to the asset load path
 # Rails.application.config.assets.paths << Emoji.images_path
 
