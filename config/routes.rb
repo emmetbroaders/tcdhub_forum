1282aec121fed55db345011a5f7955cc48b3c287
Emmet Broaders <broadere@tcd.ie>
2015-06-23 00:25:55 +0100

    created home screen using #posts for filler - working

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users
+  resources :home do
+  	resources :posts
+  end	
+
   resources :posts do
   	resources :comments
   end
 
-  root 'posts#index'
+
+  root 'home#index'
 end
